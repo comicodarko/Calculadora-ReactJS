03ab42ef1a87c0c240d51d9592efd03cc70ccd6c
Cômico <bitotajima@hotmail.com>
2020-06-20 00:53:44 -0300

    Finalizado

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -5,18 +5,67 @@ import './Calculator.css';
 import Display from '../components/Display';
 import Button from '../components/Button';
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 export default class Calculator extends Component {
 
+  state = { ...initialState }
+
   clearMemory() {
-    console.log('limpar');
+    this.setState({ ...initialState })
   }
 
   setOperation(operation) {
-    console.log(operation);
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === '=';
+      const currentOperation = this.state.operation;
+      
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      } catch(e) {
+        values[0] = this.state.values[0]
+      }
+      values[1] = 0
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals
+      })
+    }
   }
 
   addDigit(digit) {
-    console.log(digit);
+    if (digit === '.' && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay; 
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+
+    const displayValue = currentValue + digit;
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (digit !== '.') {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+
+      this.setState({ values })
+      console.log(values)
+    }
+
   }
 
   constructor(props) {
@@ -31,7 +80,7 @@ export default class Calculator extends Component {
     
     return (
       <div className="calculator">
-        <Display value={1000}/>
+        <Display value={this.state.displayValue}/>
 
         <Button label="AC" click={this.clearMemory} triple />
         <Button label="/" click={this.setOperation} operation />
